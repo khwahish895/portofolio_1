fc08341325cea46a5e785dd76e0875e369f92da1
khwahish895 <khwahishsingh2005@gmail.com>
2026-05-05 23:48:41 +0530

    Cleanup unused script files

--- fix.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'src/components/sections/Projects.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/type: "spring"/g, 'type: "spring" as any');
-content = content.replace(/type: 'spring'/g, 'type: "spring" as any');
-fs.writeFileSync(file, content);
